b5e3e8bbf7b2ec7ae9577afdd46abda0272bf50c
MD.Jahir <jahirulalam41@gmail.com>
2024-10-12 00:20:42 +0600

    Responsive design added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./static/**/*.css", "./static/**/*.js"],
+  content: [
+    "./templates/**/*.html", // Include all HTML files in the templates directory
+    "./static/**/*.css", // Include all CSS files in the static directory
+    "./static/**/*.js", // Include all JS files in the static directory
+  ],
   theme: {
+    screens: {
+      sm: "640px", // Small devices (phones)
+      md: "768px", // Medium devices (tablets)
+      lg: "1024px", // Large devices (desktops)
+      xl: "1280px", // Extra large devices (larger desktops)
+      "2xl": "1536px", // 2X larger devices (large monitors)
+    },
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui")], // DaisyUI for additional components
 };
